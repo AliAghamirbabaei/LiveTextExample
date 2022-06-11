5dde0e4e317723655ee495e00d3d99dce18de4fd
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2022-06-11 21:38:05 +0430

    Initial Commit

---
+++ LiveTextExample16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LiveTextExample16
+//
+//  Created by Ali Aghamirbabaei on 6/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
